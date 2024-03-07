7a8edd6a55c25f8d4e70dc7383b796f23b320abf
bansalyash24 <bansalyash066@gmail.com>
2024-03-07 13:57:46 +0530

    fix:url

--- assignment/src/utils.js
+++ assignment/src/utils.js
@@ -8,7 +8,7 @@ export const handleOrder = async (orderType,obj,cookies) => {
       if(!obj?.securityId) toast.error("Security Id is mandatory")
       return
     }
-    const url = "/backend/api/orders";
+    const url = "https://api.dhan.co/orders";
     const bodyObject = {
       dhanClientId: "1000475514",
       correlationId: "NA",
